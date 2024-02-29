5ea6de068bd00e21d4958d18e5f331330cf89b25
iback <hendrik.moeller@tum.de>
2024-02-29 07:58:04 +0000

    made ruffclean

--- spineps/example/helper_parallel.py
+++ spineps/example/helper_parallel.py
@@ -1,18 +1,17 @@
-import sys
+import sys  # noqa: INP001
 from pathlib import Path
 
 file = Path(__file__).resolve()
 sys.path.append(str(file.parents[1]))
 sys.path.append(str(file.parents[2]))
 
-from TPTBox import (
-    BIDS_FILE,
-)
-from spineps.models import get_segmentation_model
-from spineps.seg_run import process_img_nii
-from spineps.utils.filepaths import filepath_model
-import argparse
+import argparse  # noqa: E402
 
+from TPTBox import BIDS_FILE  # noqa: E402
+
+from spineps.models import get_segmentation_model  # noqa: E402
+from spineps.seg_run import process_img_nii  # noqa: E402
+from spineps.utils.filepaths import filepath_model  # noqa: E402
 
 if __name__ == "__main__":
     main_parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -24,14 +23,14 @@ if __name__ == "__main__":
 
     opt = main_parser.parse_args()
 
-    input = BIDS_FILE(file=opt.i, dataset=opt.ds)
+    input_bids_file = BIDS_FILE(file=opt.i, dataset=opt.ds)
 
     model_dir = "/DATA/NAS/ongoing_projects/hendrik/nako-segmentation/nnUNet/"
     ms = get_segmentation_model(in_config=filepath_model(opt.ms, model_dir=model_dir))
     mv = get_segmentation_model(in_config=filepath_model(opt.mv, model_dir=model_dir))
 
     process_img_nii(
-        img_ref=input,
+        img_ref=input_bids_file,
         derivative_name=opt.der,
         model_semantic=ms,
         model_instance=mv,
