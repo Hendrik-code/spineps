fdb39b3d536d7f30db33e3fe76a46119cf40e87c
ga84mun <robert.graf@tum.de>
2026-09-01 12:08:55 +0000

    dtye

--- spineps/phase_post.py
+++ spineps/phase_post.py
@@ -720,8 +720,8 @@ def detect_and_solve_merged_vertebra(seg_nii: NII, vert_nii: NII) -> tuple[NII, 
             # first is significantly smaller than second and they are close in height
             # how many pixels are touching
             vert_firsttwo_arr = vert_nii.extract_label(first_key).get_seg_array()
-            vert_firsttwo_arr2 = vert_nii.extract_label(second_key).get_seg_array()
-            vert_firsttwo_arr += vert_firsttwo_arr2 + 1
+            vert_firsttwo_arr2: np.ndarray = vert_nii.extract_label(second_key).get_seg_array()
+            vert_firsttwo_arr += vert_firsttwo_arr2.astype(vert_firsttwo_arr.dtype) + 1
             contacts = np_contacts(vert_firsttwo_arr, connectivity=3)
             if contacts[(1, 2)] > isotropic_area_to_voxels(MERGED_VERTEBRA_MIN_CONTACT_MM2, vert_nii.zoom):
                 logger.print("Found first two instance weird, will merge", Log_Type.STRANGE)
